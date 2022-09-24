e837d66de3744f3f093a4a87a2ab596f6ab6ca9c
orpdoji <97266005+orpdoji@users.noreply.github.com>
2022-09-23 19:00:06 -0700

    Update solution.cpp
    Fixed many syntax error, final version

--- solution.cpp
+++ solution.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
-class solution{
-    using namespace std;
+class Solution{
+
     public:
         vector<int> twoSum(vector<int>& nums, int target){
             int index1 = -1;
             int index2 = -1;
-            for(int i = 0;i<nums.length();i++){
-                for(int j = i+1; j<nums.length();j++){
+            for(int i = 0;i<nums.size();i++){
+                for(int j = i+1; j<nums.size();j++){
                     if(nums.at(i)+nums.at(j)==target){
                         index1 = i;
                         index2 = j;
@@ -20,7 +20,7 @@ class solution{
                         break;
                 }
             }
-            vector<int> res = [index1,index2];
+            vector<int> res = {index1,index2};
             return res;
         }
-}
+};
